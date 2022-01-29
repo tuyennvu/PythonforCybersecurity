7de0709583897cc3e5f7e662cfd18def8b8ee8c4
tuyennvu <vu.tuyen@student.greenriver.edu>
2022-01-28 21:18:04 -0800

    FUNCTION

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -7,3 +7,13 @@
 
 print ('Is today a good day?')
 answer = input ('y/n: ')
+
+
+def send_message(mystr):
+    print('Yes it is!')
+
+if answer == 'y':
+    print('Yes it is!')
+    for y in range(10):
+        print('Yes it is!')
+    
