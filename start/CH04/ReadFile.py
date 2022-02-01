97cadfb41f0e6aead5d308d3264874c6b197c286
tuyennvu <vu.tuyen@student.greenriver.edu>
2022-02-01 15:28:46 -0800

    readfile

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By Tuyen Vu
+
+
+
+
+hackme_file = open("hackme.txt", "r")
+file_contents = hackme_file.read()
+hackme_file.close()
+print(file_contents)
+print("Here is someone to hack - information")
+
