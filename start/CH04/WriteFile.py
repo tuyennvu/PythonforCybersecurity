df7aa24de7b61107b9f32fdf756b1dd1dcbf71d1
tuyennvu <vu.tuyen@student.greenriver.edu>
2022-02-01 15:11:47 -0800

    hackme

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -2,4 +2,16 @@
 # Sample script that writes to a file
 # By TUYEN VU 01/28/2022
 
+#open file for writing
+hackme_file = open("hackme.txt", "w")
+
+#write to the file
+hackme_file.write("What is your name?\n")
+hackme_file.write("What is your farvorite color?\n")
+hackme_file.write("What was your frist pet's name?\n")
+hackme_file.write("What is you rmother's maiden name?\n")
+hackme_file.write("What elementary school did you attend?\n")
+
+#close file
+hackme_file.close()
 
