269ae969e3aef9b299d6e89fcc99be2f8fc5e4e1
tuyennvu <vu.tuyen@student.greenriver.edu>
2022-02-21 20:17:13 -0800

    created 404

--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 # Script that scans web server logs for 404 errors
-# By 
+# By Tuyen Vu
+
+#promting for file 
+file_name = input("What file do you want to review?")
+#open file
+f = open(file_name, "r")
+#read file line by line
+while True: 
+    line = f.readline()
+    if not line:
+        break
+    #if there is a 404, print it 
+    if "404" in line:
+        print(line.strip())
+
+#close file
+f.close()
